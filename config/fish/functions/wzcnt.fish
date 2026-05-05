3938abd178d0ede4413e721aead7307d3fb6e343
Myungha Lee <myhlee02@gmail.com>
2026-05-05 21:27:52 +0900

    init

---
+++ config/fish/functions/wzcnt.fish
@@ -0,0 +1,14 @@
+function wzcnt --wraps='wezterm connect' --description 'Connect to wezterm domain in background'
+    if test (count $argv) -gt 2
+        echo "Error: too many arguments"
+    else if test (count $argv) -eq 2
+        nohup wezterm connect $argv[1] --workspace $argv[2] >/dev/null 2>&1 &
+        disown
+    else if test (count $argv) -eq 1
+        nohup wezterm connect $argv[1] --workspace dev >/dev/null 2>&1 &
+        disown
+    else
+        nohup wezterm connect localhost --workspace dev >/dev/null 2>&1 &
+        disown
+    end
+end
