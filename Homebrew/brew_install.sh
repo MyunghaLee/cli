fbde312cfb34fe77f59ccbdee0b1f7fe152d98c2
Myungha Lee <myhlee02@gmail.com>
2026-05-05 18:28:20 +0900

    init

--- Homebrew/brew_install.sh
+++ Homebrew/brew_install.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/bash
 if not test -f /home/linuxbrew/.linuxbrew/bin/brew
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 end
 
 /home/linuxbrew/.linuxbrew/bin/brew bundle
+
