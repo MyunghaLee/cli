8f5633c68d74a30cf98318aecc636c6cd60028a4
Myungha Lee <myhlee02@gmail.com>
2026-05-06 16:22:41 +0900

    init

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -5,3 +5,14 @@
 -- vim.o.clipboard = "unnamedplus"
 
 vim.opt.shell = "fish"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
