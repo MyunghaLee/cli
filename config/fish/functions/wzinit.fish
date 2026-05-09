840966d7913448be7159a87c1bf8ecbbc74f1d66
Myungha Lee <myhlee02@gmail.com>
2026-05-09 22:27:24 +0900

    init

--- config/fish/functions/wzinit.fish
+++ config/fish/functions/wzinit.fish
@@ -1,5 +1,5 @@
-function wzinit --wraps='nohup wezterm connect localhost --workspace dev 1>/dev/null 2>&1 &; disown' --description 'alias wzinit=nohup wezterm connect localhost --workspace dev 1>/dev/null 2>&1 &; disown'
-    nohup wezterm connect localhost --workspace dev 1>/dev/null 2>&1 &
-    disown
-    exit
-end
+# function wzinit --wraps='nohup wezterm connect localhost --workspace dev 1>/dev/null 2>&1 &; disown' --description 'alias wzinit=nohup wezterm connect localhost --workspace dev 1>/dev/null 2>&1 &; disown'
+#     nohup wezterm connect localhost --workspace dev 1>/dev/null 2>&1 &
+#     disown
+#     exit
+# end
