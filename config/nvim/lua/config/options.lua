cd8049a88b9989433c81efd6eb945f81ec914f59
Myungha Lee <myhlee02@gmail.com>
2026-05-12 14:53:44 +0900

    init

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,9 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.snacks_animate = false
+
 vim.opt.shell = "fish"
 
-vim.o.clipboard = "unnamedplus"
+vim.o.clipboard = "unnamedplus" -- necessary for using OSC52 in remote machine
 vim.g.clipboard = {
   name = "OSC 52",
   copy = {
