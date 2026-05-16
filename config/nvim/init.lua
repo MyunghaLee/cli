b3318b3bc97e6cefdeeb2ce72a73f8fbf4e2e484
Myungha Lee <myhlee02@gmail.com>
2026-05-17 01:47:54 +0900

    init

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -183,7 +183,7 @@ do
   vim.o.inccommand = 'split'
 
   -- Show which line your cursor is on
-  vim.o.cursorline = true
+  vim.o.cursorline = false
 
   -- Minimal number of screen lines to keep above and below the cursor.
   vim.o.scrolloff = 10
@@ -421,13 +421,42 @@ do
 
   vim.pack.add { gh 'Mofiqul/vscode.nvim' }
   ---@diagnostic disable-next-line: missing-fields
-  require('vscode').setup {
-    styles = {
-      style = 'dark',
-      transparent = true,
-      italic_comments = true,
+require('vscode').setup({
+  styles = {
+    -- Alternatively set style in setup
+    -- style = 'light'
+
+    -- Enable transparent background
+    transparent = true,
+
+    -- Enable italic comment
+    italic_comments = true,
+
+    -- Enable italic inlay type hints
+    italic_inlayhints = true,
+
+    -- Underline `@markup.link.*` variants
+    underline_links = true,
+
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = true,
+
+    -- Apply theme colors to terminal
+    terminal_colors = true,
+
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+        vscLineNumber = '#FFFFFF',
     },
-  }
+
+    -- Override highlight groups (see ./lua/vscode/theme.lua)
+    group_overrides = {
+        -- this supports the same val table as vim.api.nvim_set_hl
+        -- use colors from this colorscheme by requiring vscode.colors!
+    },
+  },
+  transparent = true,
+})
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
