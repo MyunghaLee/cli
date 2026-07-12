8ca22adcfbee6678e631244b2ba30b6603053362
Myungha Lee <myhlee02@gmail.com>
2026-07-13 01:07:07 +0900

    init

--- link.sh
+++ link.sh
@@ -1,20 +1,3 @@
-# ARCH=$(uname -m)
-# case "$ARCH" in
-# x86_64 | amd64)
-#   FISH="fish-x86_64"
-#   ZELLIJ="zellij-x86_64"
-#   ;;
-# aarch64 | arm64)
-#   FISH="fish-aarch64"
-#   ZELLIJ="zellij-aarch64"
-#   ;;
-# *)
-#   exit 1
-#   ;;
-# esac
-# ln -sf $PWD/bin/$FISH $PWD/bin/fish
-# ln -sf $PWD/bin/$ZELLIJ $PWD/bin/zellij
-
 mkdir -p $HOME/.local/bin
 ln -sf $PWD/bin/* $HOME/.local/bin
 
